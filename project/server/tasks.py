009013811a5dab2f80c10530ab801a0454d856db
AndrewLien <37082744+AndrewLien@users.noreply.github.com>
2022-05-19 16:12:49 -0500

    allow tasks.py to take scrapenames

--- project/server/tasks.py
+++ project/server/tasks.py
@@ -10,6 +10,6 @@ celery.conf.result_backend = os.environ.get("CELERY_RESULT_BACKEND", "redis://lo
 
 
 @celery.task(name="create_task")
-def create_task(task_type):
-    time.sleep(int(task_type) * 10)
+def create_task(scrapename):
+    exec(open('project/scrapes/' + scrapename).read())
     return True
