f761ea2138a2c7dea469d9a589e99f9aa1abec6f
lienan01 <andrew.lien@lexisnexisrisk.com>
2022-05-24 10:00:52 -0500

    logging testing

--- project/scrapes/test.py
+++ project/scrapes/test.py
@@ -1,6 +1,7 @@
 import requests
 import datetime
-
+import logging
+import sys
 r = requests.get('https://pypistats.org/top')
 data = r.text
 
@@ -9,6 +10,17 @@ print(filename)
 
 now = datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
 print(now)
+
+logging.basicConfig(
+    format='%(asctime)s|%(name)s:%(lineno)s|%(levelname)s|%(message)s',
+    level=logging.DEBUG,
+    stream=sys.stdout,
+)
+
+logger = logging.getLogger('test.py')
+
+logger.debug('this is level debug')
+logger.info('this is level info')
 
 with open('project/output/' + filename + '_' + now, 'w+') as f:
     f.write(data)
--- project/server/tasks.py
+++ project/server/tasks.py
@@ -1,15 +1,35 @@
 import os
-import time
+import psutil
+import subprocess
+import logging
 
 from celery import Celery
+import sys
+import datetime
+
+now = datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
 
 
 celery = Celery(__name__)
 celery.conf.broker_url = os.environ.get("CELERY_BROKER_URL", "redis://localhost:6379")
 celery.conf.result_backend = os.environ.get("CELERY_RESULT_BACKEND", "redis://localhost:6379")
 
+logger = logging.getLogger('celery-app.tasks')
+fh = logging.FileHandler('project/logs/' + now + '.log')
+logger.addHandler(fh)
 
 @celery.task(name="create_task")
 def create_task(scrapename):
-    exec(open('project/scrapes/' + scrapename).read())
+    # exec(open('project/scrapes/' + scrapename).read())
+    # files = psutil.Process().open_files()  # this only returns celery log files
+    # with open('files.txt', 'w') as f:
+    #     f.write(str(f))
+    cmd = [sys.executable, 'project/scrapes/' + scrapename]
+
+    p = subprocess.Popen(cmd, universal_newlines=True,
+                         # stdout=log_file,
+                         stdout=subprocess.PIPE,
+                         stderr=subprocess.STDOUT)  # TODO tweak
+    stdout, _ = p.communicate()
+    logger.log(logging.INFO, stdout)
     return True
